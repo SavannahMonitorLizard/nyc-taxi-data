055ef84298447d1203b3ca6d89efa4dec154c533
Tymur Tkachenko <tymur.tk@gmail.com>
2022-09-06 14:57:09 -0400

    pandas being bad

--- pq_to_mysql_all.py
+++ pq_to_mysql_all.py
@@ -3,6 +3,7 @@ from os.path import exists
 from sqlalchemy import create_engine
 import json
 import pyarrow.parquet as pq
+import mysql.connector
 
 with open("config.json") as json_file:
     config = json.load(json_file)
@@ -12,16 +13,14 @@ with open("config.json") as json_file:
     uname = config["username"]
     pwd = config["password"]
 
-year = ""
-month = ""
-
 def main():
     for year in ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]:
         for month in ["01", "02", "03", "04", "05", "07", "08", "09", "10", "11", "12"]:
             if exists(f"{year}_{month}_taxidata.parquet"):
-                trips = pq.read_table(f'{year}_{month}_taxidata.parquet', coerce_int96_timestamp_unit="us")
+                trips = pq.read_table(f'{year}_{month}_taxidata.parquet', 
+                                        coerce_int96_timestamp_unit="us")
                 trips = trips.to_pandas()
-
+                
                 engine = create_engine(f"mysql+pymysql://{uname}:{pwd}@{hostname}/{dbname}")
                 trips.to_sql(con=engine, name=f'{year}_{month}', if_exists='replace')
 
